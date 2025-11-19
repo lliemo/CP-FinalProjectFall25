b8371d1e383c6792a052471e6680985d17a6d104
lliemo <halliemmx@gmail.com>
2025-11-19 18:26:59 -0500

    no errors but the crashWith() function doesn't work

--- Project Check-in/Game Code/app.js
+++ Project Check-in/Game Code/app.js
@@ -68,73 +68,107 @@ var myGameArea = {
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
+  stop: function () {
+    clearInterval(this.interval);
+  },
 };
 
-function component(width, height, color, x, y, type) {
-  this.type = type;
-  this.score = 0;
-  this.width = width;
-  this.height = height;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.x = x;
-  this.y = y;
-  this.myNotes = [
-    "A",
-    "A#",
-    "B",
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-  ];
-  this.text = this.myNotes[Math.floor(Math.random() * this.myNotes.length)];
-  //this.gravity = 0;
-  //this.gravitySpeed = 0;
-  this.update = function () {
+class component {
+  constructor(width, height, color, x, y, type) {
+    this.type = type;
+    this.score = 0;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.myObstacles = myObstacles;
+    this.myNotes = [
+      "A",
+      "A#",
+      "B",
+      "C",
+      "C#",
+      "D",
+      "D#",
+      "E",
+      "F",
+      "F#",
+      "G",
+      "G#",
+    ];
+    this.text = this.myNotes[Math.floor(Math.random() * this.myNotes.length)];
+    //this.gravity = 0;
+    //this.gravitySpeed = 0;
+  }
+  update = () => {
     let ctx = myGameArea.context;
     if (this.type == "text") {
       ctx.font = this.width + " " + this.height;
-      ctx.fillStyle = color;
+      ctx.fillStyle = this.color;
       ctx.fillText(this.text, this.x, this.y);
     } else {
-      ctx.fillStyle = color;
+      ctx.fillStyle = this.color;
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-
-    this.crashWith = function (otherobj) {
-      var myleft = this.x;
-      var myright = this.x + this.width;
-      var mytop = this.y;
-      var mybottom = this.y + this.height;
-      var otherleft = otherobj.x;
-      var otherright = otherobj.x + otherobj.width;
-      var othertop = otherobj.y;
-      var otherbottom = otherobj.y + otherobj.height;
-      var crash = true;
-      if (
-        mybottom < othertop ||
-        mytop > otherbottom ||
-        myright < otherleft ||
-        myleft > otherright
-      ) {
-        crash = false;
-      }
-      return crash;
-    };
   };
-  this.newPos = function () {
+
+  // crashWith = (otherobj) => {
+  //   var otherobj = otherobj;
+  //   var x = this.x;
+  //   var myleft = this.x;
+  //   var myright = this.x + this.width;
+  //   var mytop = this.y;
+  //   var mybottom = this.y + this.height;
+  //   var otherleft = otherobj.x;
+  //   var otherright = otherobj.x + otherobj.width;
+  //   var othertop = otherobj.y;
+  //   var otherbottom = otherobj.y + otherobj.height;
+  //   var crash = true;
+  //   if (
+  //     mybottom < othertop ||
+  //     mytop > otherbottom ||
+  //     myright < otherleft ||
+  //     myleft > otherright
+  //   ) {
+  //     crash = false;
+  //   }
+
+  //   return crash;
+  // };
+
+  crashWith = () => {
+    var myleft = this.x;
+    var myright = this.x + this.width;
+    var mytop = this.y;
+    var mybottom = this.y + this.height;
+    var otherleft = this.myObstacles.x;
+    var otherright = this.myObstacles.x + this.myObstacles.width;
+    var othertop = this.myObstacles.y;
+    var otherbottom = this.myObstacles.y + this.myObstacles.height;
+    var crash = true;
+    if (
+      myleft < otherright &&
+      myright > otherleft &&
+      mytop < otherbottom &&
+      mybottom > othertop
+    ) {
+      console.log("crash");
+      return crash;
+    }
+
+    crash = false;
+  };
+
+  newPos = function () {
     //this.gravitySpeed += this.gravity;
     this.x += this.speedX;
     this.y += this.speedY; //+ this.gravitySpeed;
     this.hitBottom();
   };
-  this.hitBottom = function () {
+  hitBottom = function () {
     var rockbottom = myGameArea.canvas.height - this.height;
     if (this.y > rockbottom) {
       this.y = rockbottom;
@@ -144,6 +178,7 @@ function component(width, height, color, x, y, type) {
 }
 
 function updateGameArea() {
+  console.log(myGamePiece);
   var x, height, gap, minHeight, maxHeight, minGap, maxGap;
   // for (let i = 0; i < myObstacles.length; i += 1) {}
 
@@ -174,10 +209,20 @@ function updateGameArea() {
     // velocity *= 1.05;
     myObstacles[i].update();
   }
-  // if (myGamePiece.crashWith(myObstacles)) {
-  //   console.log("crash");
+  if (myGamePiece.crashWith()) {
+    console.log("crash");
+    // myGameArea.stop();
+    // } else {
+    //   myGameArea.clear();
+    //   myObstacle.update();
+    //   myGamePiece.newPos();
+    //   myGamePiece.update();
+  }
+  //-------------------------------
+  // if (myMIDIstuff.onNoteOn.pitch == midiNum2NoteName(myObstacles.text)) {
+  //   console.log("correct note played, continue game");
   // } else {
-  //   console.log("no crash");
+  //   console.log("wrong note");
   //   // myGameArea.clear();
   //   // myObstacle.update();
   //   // myGamePiece.newPos();
@@ -291,11 +336,11 @@ myMIDIstuff.onNoteOff = (pitch, velocity, ch) => {
 // }
 
 //FYI I MADE UP noteText and gamePieceInGreen, you need to replace with those values
-if (midiNum2NoteName(pitch) == myObstacles.text && myGamePiece == true) {
-  //then do stuff
-} else {
-  stopMove();
-}
+// if (midiNum2NoteName(pitch) == myObstacles.text && myGamePiece == true) {
+//   //then do stuff
+// } else {
+//   stopMove();
+// }
 
 //-----------------------------------------MIDI Sound-----------------------------------------
 
